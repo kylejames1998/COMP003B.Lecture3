6aa78e090824fe6571cdfbeee2f2315ebc74b7cc
kylejames1998 <kylesacct409@outlook.com>
2024-04-08 19:49:43 -0700

    added Edit feature

--- COMP003B.Lecture3/Controllers/StudentsController.cs
+++ COMP003B.Lecture3/Controllers/StudentsController.cs
@@ -13,5 +13,85 @@ namespace COMP003B.Lecture3.Controllers
         {
             return View(_students);
         }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sudents/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            // TODO: add model state validation
+            if (ModelState.IsValid)
+            {
+                // TODO: check if student already exists 
+                if (!_students.Any(s => s.Id == student.Id))
+                {
+                    // TODO: add student to list
+                    _students.Add(student);
+                }
+                
+                // TODO: redirect back to index
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        // GET: Students/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            // TODO: Check if id is null
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // TODO: find student by id
+            var student = _students.FirstOrDefault(p => p.Id == id);
+
+            // TODO: Check agin if student is null after searching the list
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            // TODO: return student to view
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Student student)
+        {
+            // TODO: check if id is the same as student id
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // TODO: search for student in list
+                var existingStudent = _students.FirstOrDefault(s => s.Id == student.Id);
+
+                // TODO: check if student found
+                if (existingStudent != null)
+                {
+                    // TODO: update student details
+                    existingStudent.Name = student.Name;
+                    existingStudent.Age = student.Age;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(student);
+        }
     }
 }
--- COMP003B.Lecture3/Models/Student.cs
+++ COMP003B.Lecture3/Models/Student.cs
@@ -11,6 +11,7 @@ namespace COMP003B.Lecture3.Models
         public int Id { get; set; }
         [Required]
         [StringLength(100)]
+        [Display(Name="Full Name")]
         public string Name { get; set; }
         [Required]
         [Range(0, 120)]
